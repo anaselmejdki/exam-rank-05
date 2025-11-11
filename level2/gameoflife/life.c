54a94de7cfa74177d9b9803e9a603358c96a12b3
ael-mejd <ael-mejd@c4r10p6.1337.ma>
2025-11-12 00:52:03 +0100

    done

--- level2/gameoflife/life.c
+++ level2/gameoflife/life.c
@@ -1,25 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <unistd.h>
 
 int main(int ac, char **av)
 {
     if (ac != 4)
-        return 1;
+        return (printf("Bad number of arguments\n"), 1);
     int w = atoi(av[1]), h = atoi(av[2]), iter = atoi(av[3]), x = 0, y = 0;
-    if (h <= 0 || w <= 0 || iter < 0)
-        return 1;
-    int board[h][w], next[w][h];
+    if (w <= 0 || h <= 0 || w < 0)
+        return (printf("Wrong arguments.\n"), 1);
+    int board[h][w], next[h][w];
     bool pen = false;
     char c;
 
     for (int i = 0; i < h; i++)
-    {
         for (int j = 0; j < w; j++)
             board[i][j] = 0;
-    }
-
-    while (read(1, &c, 0) == 1)
+    
+    while (read(0, &c, 1) == 1)
     {
         if (c == 'w' && x > 0) x--;
         else if (c == 's' && x < h - 1) x++;
@@ -33,17 +32,31 @@ int main(int ac, char **av)
     {
         for (int i = 0; i < h; i++)
         {
-            for (int j = 0; j , w; j++)
+            for (int j = 0; j < w; j++)
             {
                 int n = 0;
                 for (int xi = -1; xi <= 1; xi++)
                 {
-                    for (int xj = -1; xi <= 1; xj++)
-                    {
-                        if ((xj || xi) && (xi ))
-                    }
+                    for (int xj = -1; xj <= 1; xj++)
+                        if ((xi || xj) && (xi + i >= 0 && xi + i < h && xj + j >= 0 && xj + j < h))
+                            n += board[i + xi][j + xj];
                 }
+                next[i][j] = ((board[i][j] && (n == 2 || n == 3)) || !board[i][j] && n == 3);
             }
         }
+        for (int i = 0; i < h; i++)
+        {
+            for (int j = 0; j < w; j++)
+                board[i][j] = next[i][j];
+        }
+    }
+
+    for (int i = 0; i < h; i++)
+    {
+        for (int j = 0; j < w; j++)
+        {
+            board[i][j] ? putchar('0') : putchar(' ');
+        }
+        putchar('\n');
     }
 }
